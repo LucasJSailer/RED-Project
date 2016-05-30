71b584848ab6b8fe13fc7deb2e23deeeff24a84c
Lucas Sailer <LucasJSailer@gmail.com>
2016-05-29 21:57:25 -0700

    Project 2

---
+++ open_close.js
@@ -0,0 +1,102 @@
+// JavaScript Document
+
+function dropdown()
+{        
+    document.getElementById("dropdown").style.display="block";
+}
+
+function dropdownclose()
+{        
+    document.getElementById("dropdown").style.display="none";
+}
+
+<!-- Portfolio expand / close buttons --->
+
+   function showFWC() { 
+        if(document.getElementById('portfolio-item-display-container-FWC').style.display=='none') { 
+            document.getElementById('portfolio-item-display-container-FWC').style.display='block';
+			document.getElementById('expand-button-container-FWC').style.display='none'; 
+			document.getElementById('expand-button-container-FWC-1').style.display='block';  
+        } 
+        return false;
+    } 
+	
+    function hideFWC() { 
+        if(document.getElementById('portfolio-item-display-container-FWC').style.display=='block') { 
+            document.getElementById('portfolio-item-display-container-FWC').style.display='none';
+			document.getElementById('expand-button-container-FWC').style.display='block'; 
+			document.getElementById('expand-button-container-FWC-1').style.display='none'; 
+        } 
+        return false;
+    }   
+
+    function showBDC() { 
+        if(document.getElementById('portfolio-item-display-container-BDC').style.display=='none') { 
+            document.getElementById('portfolio-item-display-container-BDC').style.display='block';
+			document.getElementById('expand-button-container-BDC').style.display='none'; 
+			document.getElementById('expand-button-container-BDC-1').style.display='block';  
+        } 
+        return false;
+    }
+	
+    function hideBDC() { 
+        if(document.getElementById('portfolio-item-display-container-BDC').style.display=='block') { 
+            document.getElementById('portfolio-item-display-container-BDC').style.display='none';
+			document.getElementById('expand-button-container-BDC').style.display='block'; 
+			document.getElementById('expand-button-container-BDC-1').style.display='none'; 
+        } 
+        return false;
+    }   
+
+    function showPP() { 
+        if(document.getElementById('portfolio-item-display-container-PP').style.display=='none') { 
+            document.getElementById('portfolio-item-display-container-PP').style.display='block';
+			document.getElementById('expand-button-container-PP').style.display='none'; 
+			document.getElementById('expand-button-container-PP-1').style.display='block';  
+        } 
+        return false;
+    } 
+    function hidePP() { 
+        if(document.getElementById('portfolio-item-display-container-PP').style.display=='block') { 
+            document.getElementById('portfolio-item-display-container-PP').style.display='none';
+			document.getElementById('expand-button-container-PP').style.display='block'; 
+			document.getElementById('expand-button-container-PP-1').style.display='none'; 
+        } 
+        return false;
+    }   
+
+    function showCC() { 
+        if(document.getElementById('portfolio-item-display-container-CC').style.display=='none') { 
+            document.getElementById('portfolio-item-display-container-CC').style.display='block';
+			document.getElementById('expand-button-container-CC').style.display='none'; 
+			document.getElementById('expand-button-container-CC-1').style.display='block';  
+        } 
+        return false;
+    } 
+	
+    function hideCC() { 
+        if(document.getElementById('portfolio-item-display-container-CC').style.display=='block') { 
+            document.getElementById('portfolio-item-display-container-CC').style.display='none';
+			document.getElementById('expand-button-container-CC').style.display='block'; 
+			document.getElementById('expand-button-container-CC-1').style.display='none'; 
+        } 
+        return false;
+    }   
+
+    function showDM() { 
+        if(document.getElementById('portfolio-item-display-container-DM').style.display=='none') { 
+            document.getElementById('portfolio-item-display-container-DM').style.display='block';
+			document.getElementById('expand-button-container-DM').style.display='none'; 
+			document.getElementById('expand-button-container-DM-1').style.display='block';  
+        } 
+        return false;
+    } 
+	
+    function hideDM() { 
+        if(document.getElementById('portfolio-item-display-container-DM').style.display=='block') { 
+            document.getElementById('portfolio-item-display-container-DM').style.display='none';
+			document.getElementById('expand-button-container-DM').style.display='block'; 
+			document.getElementById('expand-button-container-DM-1').style.display='none'; 
+        } 
+        return false;
+    }   
